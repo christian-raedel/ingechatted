d4e6c7dcf1c869d7eecf3f3f4bc6602a6836fa33
Christian Rädel <christian.raedel@gmail.com>
2014-04-11 23:19:52 +0200

    Added start-script.

--- run-app.js
+++ run-app.js
@@ -1,9 +1,27 @@
 (function () {
     'use strict';
 
-    var server = require('./lib/app').createServer();
+    var spawn = require('child_process').spawn,
+        config = require('./lib/config'),
+        debug = require('debug')(config.name + ':run-app'),
+        app = require('./lib/app');
 
-    server.on('shutdown', function(reason) {
-        process.exit(reason);
+    if (!config.crossbar) {
+        throw new Error('please specify crossbar executable');
+    }
+
+    var crossbar = spawn(config.crossbar, ['start'], { cwd: __dirname });
+    crossbar.stdout.on('data', debug);
+    crossbar.stderr.on('data', debug);
+    crossbar.on('close', function() {
+        server.shutdown();
     });
+
+    var timeout = setTimeout(function() {
+        var server = app.createServer();
+        server.on('shutdown', function () {
+            process.exit(0);
+        });
+        clearTimeout(timeout);
+    }, 5000);
 }(this));
